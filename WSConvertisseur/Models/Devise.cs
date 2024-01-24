f066f5f2462ed271e3a75e856b518745f848813b
SIMON GOY <simon.goy@etu.univ-smb.fr>
2024-01-24 15:00:00 +0100

    Ajour du modèle et modif paramètres

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,50 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomdevise;
+        private double taux;
+
+        public Devise(int id, string? nomdevise, double taux)
+        {
+            Id = id;
+            Nomdevise = nomdevise;
+            Taux = taux;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string? Nomdevise
+        {
+            get
+            {
+                return nomdevise;
+            }
+            set
+            {
+                nomdevise = value;
+            }
+        }
+        public double Taux
+        {
+            get
+            {
+                return taux;
+            }
+            set
+            {
+                taux = value;
+            }
+        }
+    }
+}
